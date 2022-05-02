746e7136130a4dbf86b6a24984087d371911c804
choudel <chouaibh@gmail.com>
2022-05-02 05:13:56 +0100

    https integration done

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 //primary file for the API
 
 const http = require("http");
+const https = require("https");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 let config = require("./configuration");
-let server = http.createServer(function (req, res) {
+const fs = require("fs");
+
+let httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+const httpsServerOptions = {
+  key: fs.readFileSync("./https/privatekey.pem"),
+  cert: fs.readFileSync("./https/certificate.pem"),
+};
+let httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+httpServer.listen(config.httpPort, function () {
+  console.log("listening on port " + config.httpPort);
+});
+httpsServer.listen(config.httpsPort, function () {
+  console.log("listening on port " + config.httpsPort);
+});
+
+//unified serer
+
+const unifiedServer = function () {
   const parseUrl = url.parse(req.url, true);
   const path = parseUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
   const queryStringObject = parseUrl.pathname;
   const method = req.method.toLocaleLowerCase();
-
   const headers = req.headers;
-
   const decoder = new StringDecoder("utf-8");
   let buffer = "";
 
@@ -20,6 +42,7 @@ let server = http.createServer(function (req, res) {
     buffer += decoder.write(data);
   });
   req.on("end", function () {
+   
     buffer += decoder.end();
 
     const chosenHandler =
@@ -40,20 +63,17 @@ let server = http.createServer(function (req, res) {
 
       var payloadString = JSON.stringify(payload);
 
-      res.setHeader('Content-Type','application/json')
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(statusCode);
       res.end(payloadString);
 
       console.log("the response returned is ", statusCode, payloadString);
     });
   });
-});
-
-server.listen(config.port, function () {
-  console.log("listening on port "+ config.port +" in "+config.envName);
-});
+};
 
 // the router
+
 var handlers = {};
 handlers.sample = function (data, callback) {
   callback(406, { name: "sample handler" });
